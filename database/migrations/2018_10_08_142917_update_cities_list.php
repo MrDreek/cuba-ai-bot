4ed6b03f4ef2a8ea8ea5e3bae0abded92e249893
nikita.klyosow <nikitka2904@gmail.com>
2018-10-08 17:32:06 +0300

    fix xml

---
+++ database/migrations/2018_10_08_142917_update_cities_list.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateCitiesList extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $path = storage_path() . '/app/additional_cities.json';
+        $json = json_decode(file_get_contents($path));
+
+        foreach ($json as $item) {
+            $city = \App\City::where('name', $item->name)->get();
+            foreach ($city as $i) {
+                $i->delete();
+            }
+            $city = new \App\City;
+            $city->name = $item->name;
+            $city->location = $item->location;
+            $city->iata = $item->iata;
+            $city->save();
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
